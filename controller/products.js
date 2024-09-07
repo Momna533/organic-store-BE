c5f996f739d139979036284ff164ff35f08c435c
Momna Ijaz <momna533gb@gmail.com>
2024-09-07 19:19:30 +0500

    name filter done

--- controller/products.js
+++ controller/products.js
@@ -1,9 +1,15 @@
 const productModel = require("../model/productModel");
 
 const getAllProducts = async (req, res) => {
+  const { name } = req.query;
+  const queryObject = {};
+  if (name) {
+    queryObject.name = new RegExp(name, "i");
+  }
   try {
-    const products = await productModel.find({});
-    res.status(200).json({ products });
+    const products = await productModel.find(queryObject).sort("name -price");
+
+    res.status(200).json({ nbHits: products.length, products });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
   }
